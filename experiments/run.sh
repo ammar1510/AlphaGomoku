38ac0fca3a9723c3fe4f504e610b1fe8279ee67f
Ammar3 Shaikh <ammarshaikh1510@gmail.com>
2025-05-31 10:24:20 +0530

    training fixes

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
 
 
+pid=$(ps aux | grep '[u]v run python -m alphagomoku.training.train_ppo_versus' | awk '{print $2}')
+
+if [ -n "$pid" ]; then
+    echo "Killing process $pid"
+    kill -9 $pid
+fi
+
+
 export PATH="$HOME/.local/bin:$PATH"
 export WANDB_API_KEY="f96aad01a13c399670f72c989b19a0b7952ae6b1"
 
@@ -12,4 +20,4 @@ git pull origin sharding
 
 . .venv/bin/activate
 
-uv run python -m alphagomoku.training.train_ppo_versus
+nohup uv run python -m alphagomoku.training.train_ppo_versus &
